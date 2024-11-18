318f4a58f10c86798579f653d9b990dce353c7cd
ByteXenon <125568681+ByteXenon@users.noreply.github.com>
2024-11-18 10:17:22 -0700

    refactor(compiler): move example comments inside `IRInstruction` class

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -1,6 +1,14 @@
 /* IRInstruction */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class IRInstruction {
+  /* Example:
+  MOVE R0, R1 ; Two registers
+  LOADK R0, K0 ; Register and constant
+  ADD R0, K0, K1 ; Register (dest) and two constants
+  CLOSURE R0, P0 ; Register and prototype
+  JMP OFFSET-10 ; Jump with offset (relative)
+  */
+
   public opcode: string;
   public operands: IROperand[];
 
@@ -18,13 +26,3 @@ class IROperand {
     this.value = value;
   }
 }
-
-/* Example:
-
-  MOVE R0, R1 ; Two registers
-  LOADK R0, K0 ; Register and constant
-  LOADK R0, K0, K1 ; Register (dest) and two constants
-  CLOSURE R0, P0 ; Register and prototype
-  JMP OFFSET-10 ; Jump with offset (relative)
-
-*/
